1eb90e7bfe1dd05fe439d1af7051cf0eb00d1e82
vasily <post_lv@mail.ru>
2022-05-17 20:38:43 +0300

    add post details

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     text = models.TextField(max_length=1000)
     image = models.ImageField(upload_to='post_images/')
 
+    def get_summary(self):
+        return self.text[:70] + '...'
+
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_blog, name='show_blog'),
+    path('<int:post_id>/', views.post_details, name='post_details')
 ]
