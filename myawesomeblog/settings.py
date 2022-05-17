b8e054915e28c72d875eae65d80af6576bb0d565
vasily <post_lv@mail.ru>
2022-05-17 21:17:32 +0300

    final

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -124,7 +124,11 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'myawesomeblog/static/'
+]
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
